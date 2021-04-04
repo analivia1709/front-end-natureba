d6bff6da257f1074920d1f9a3351b477748d0025
Majoralphax <Riltonsp22@gmail.com>
2021-04-04 18:13:37 -0300

    Minha parte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { AlterarcadastroComponent } from './alterarcadastro/alterarcadastro.component';
+import { AlterarprodutoComponent } from './alterarproduto/alterarproduto.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { CardComponent } from './card/card.component';
+import { CriarprodutoComponent } from './criarproduto/criarproduto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    AlterarcadastroComponent,
+    AlterarprodutoComponent,
+    CadastrarComponent,
+    CardComponent,
+    CriarprodutoComponent
   ],
   imports: [
     BrowserModule,

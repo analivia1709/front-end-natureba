fd67696eb51ff6f6049fcda3581d5f482616696f
Majoralphax <Riltonsp22@gmail.com>
2021-04-07 18:27:37 -0300

    Minhas atualizaçao

---
+++ src/app/pagina-produto-vendedor/pagina-produto-vendedor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-produto-vendedor',
+  templateUrl: './pagina-produto-vendedor.component.html',
+  styleUrls: ['./pagina-produto-vendedor.component.css']
+})
+export class PaginaProdutoVendedorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
